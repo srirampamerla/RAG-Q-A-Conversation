436d4944ade941db5f4da56a85adc8315987ef8a
srirampamerla <53964156+srirampamerla@users.noreply.github.com>
2024-08-30 10:45:38 +0530

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,19 @@
+langchain-openai
+langchain
+python-dotenv
+langchain_community
+langchain_core
+streamlit
+langchain_groq
+fastapi
+pypdf
+bs4
+pymupdf
+# arxiv
+# wikipedia
+langchain-text-splitters
+# chromadb
+sentence_transformers
+langchain_huggingface
+faiss-cpu
+# langchain_chroma
